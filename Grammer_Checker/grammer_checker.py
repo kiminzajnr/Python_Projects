974c5858fea40f2e872d692dbb557332718c7d30
kiminzajnr <kiminzaeric@gmail.com>
2024-01-27 22:56:53 +0300

    add option

--- Grammer_Checker/grammer_checker.py
+++ Grammer_Checker/grammer_checker.py
@@ -2,7 +2,7 @@ import language_tool_python
 
 
 def grammar_checker(text):
-    tool = language_tool_python.LanguageTool('en-US')
+    tool = language_tool_python.LanguageTool('en-US', config={ 'cacheSize': 1000, 'pipelineCaching': True })
     matches = tool.check(text)
 
 
@@ -15,10 +15,13 @@ def grammar_checker(text):
         print("No grammar issues found")
         return
 
-    print(f"\nThe corrected sentence is:\n{tool.correct(text)}")
+    print(f"\nThe corrected text is:\n{tool.correct(text)}")
 
 
 if __name__ == "__main__":
-    text_to_check = input("Enter the text you want to check for grammar: ")
-    grammar_checker(text_to_check)
+    print("Options:\n1: To check a text\n2: To check a file")
+    user_selection = input("Enter your option: ")
+    if user_selection == "1":
+        text_to_check = input("Enter the text you want to check for grammar: ")
+        grammar_checker(text_to_check)
     
