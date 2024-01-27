0973d649899be295099f913803a8e150c366323b
kiminzajnr <kiminzaeric@gmail.com>
2024-01-27 23:40:48 +0300

    add ability to read from file & test file

--- Grammer_Checker/grammer_checker.py
+++ Grammer_Checker/grammer_checker.py
@@ -17,11 +17,26 @@ def grammar_checker(text):
 
     print(f"\nThe corrected text is:\n{tool.correct(text)}")
 
+def read_from_file(filename):
+    with open(filename) as file:
+        text_to_check = file.read()
+    grammar_checker(text_to_check)
+
 
 if __name__ == "__main__":
-    print("Options:\n1: To check a text\n2: To check a file")
-    user_selection = input("Enter your option: ")
-    if user_selection == "1":
-        text_to_check = input("Enter the text you want to check for grammar: ")
-        grammar_checker(text_to_check)
-    
+    while True:
+        print("\nOptions:\n1: To check a text\n2: To check a file\n3: Quit")
+        user_selection = input("\nEnter your option: ")
+
+        if user_selection == "1":
+            text_to_check = input("Enter the text you want to check for grammar: ")
+            grammar_checker(text_to_check)
+
+        elif user_selection == "2":
+            file_name = input("Enter file name: ")
+            read_from_file(file_name)
+
+        elif user_selection == "3":
+            print("Goodbye!")
+            break
+        
