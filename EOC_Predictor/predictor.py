5e32993a4ede633dbda81cb04ae745743b5ca0bf
kiminzajnr <kiminzaeric@gmail.com>
2024-01-30 22:08:27 +0300

    define factors and their prob

--- EOC_Predictor/predictor.py
+++ EOC_Predictor/predictor.py
@@ -1,5 +1,25 @@
 def end_of_civilization_predictor():
     print("Welcome to the End of Civilizaton Predictor!!")
 
+    # factors and probabilities
+    factors = {
+        "Climate Change": 0.3,
+        "Overpopulation": 0.2,
+        "Probability of War": 0.25,
+        "Deadly Pathogens": 0.15,
+        "Technological Catastrophe": 0.1,
+    }
+
+    total_probability = 0
+    for prob in factors.values():
+        total_probability += prob
+
+    print("\nFactors and Their Probabilities:")
+    for factor, probability in factors.items():
+        print(f"{factor}: {probability * 100}%")
+
+    print("\nTotal Probability:", total_probability)
+
+
 if __name__ == "__main__":
     end_of_civilization_predictor()
