7f18b2e94bcdf4d3bd39e156300883cc7582a056
kiminzajnr <kiminzaeric@gmail.com>
2024-02-02 00:09:56 +0300

    list files & deleted test files

--- File_Organizer/file_organizer.py
+++ File_Organizer/file_organizer.py
@@ -1,7 +1,7 @@
 # Create a script that organizes files in a directory based on their types (e.g., images, documents, videos).
 # You can use file extensions or content analysis to determine file types.
 
-from os import path, makedirs
+from os import path, makedirs, listdir
 
 def create_destination_dirs(destination_dir, file_types):
     """
@@ -10,6 +10,16 @@ def create_destination_dirs(destination_dir, file_types):
     for file_type in file_types:
         dir_path = path.join(destination_dir, file_type)
         makedirs(dir_path, exist_ok=True)
+
+def list_files_in_dir(source_dir):
+    """
+    List files in the source directory.
+    """
+    print(f"\nFiles in {source_dir}:")
+    for filename in listdir(source_dir):
+        file_path = path.join(source_dir, filename)
+        if path.isfile(file_path):
+            print(f"- {filename}")
 
 
 def main():
@@ -22,6 +32,9 @@ def main():
 
     # create destination directories
     create_destination_dirs(destination_dir, file_types)
+
+    # list files in the source dir
+    list_files_in_dir(source_dir)
 
 if __name__ == "__main__":
     main()
