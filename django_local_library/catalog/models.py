d2b7329bcc294fcd8ce02b1002b3baaaed574b5f
kiminzajnr <kiminzaeric@gmail.com>
2024-04-28 15:08:30 +0300

    complete models

--- django_local_library/catalog/models.py
+++ django_local_library/catalog/models.py
@@ -1,8 +1,9 @@
+import uuid
 from django.urls import reverse
 from django.db import models
 
 
-from django.db import UniqueConstraint
+from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
 
 class Genre(models.Model):
@@ -46,3 +47,70 @@ class Genre(models.Model):
         
         def get_absolute_url(self):
             """Returns the url to access a detail record of this book"""
+
+
+    class BookInstance(models.Model):
+        """specific copy of a book"""
+        id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                              help_text="Unique ID for this particula book across whole library")
+        book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+        imprint = models.CharField(max_length=200)
+        due_back = models.DateField(null=True, blank=True)
+
+
+        LOAN_STATUS = (
+            ('m', 'Maintenance'),
+            ('o', 'On Loan'),
+            ('a', 'Available'),
+            ('r', 'Reserved'),
+        )
+        status = models.CharField(
+            max_length=1,
+            choices=LOAN_STATUS,
+            blank=True,
+            default='m',
+            help_text='Book availability',
+        )
+
+
+        class Meta:
+            ordering = ['due_back']
+
+        def __str__(self):
+            return f'{self.id} ({self.book.title})'
+        
+
+    class Author(models.Model):
+        first_name = models.CharField(max_length=100)
+        last_name = models.CharField(max_length=100)
+        date_of_birth = models.DateField(null=True, blank=True)
+        date_of_death = models.DateField('Died', null=True, blank=True)
+
+        class Meta:
+            ordering = ['last_name', 'first_name']
+
+        def get_absolute_url(self):
+            return reverse('author-detail', args=[str(self.id)])
+        
+        def __str__(self):
+            return f'{self.last_name}, {self.first_name}'
+        
+
+    class Language(models.Model):
+        name = models.CharField(max_length=200, unique=True, help_text="Enter books natural language")
+        
+
+        def get_absolute_url(self):
+            return reverse('language-detail', args=[str(self.id)])
+        
+        def __str__(self):
+            return self.name
+        
+        class Meta:
+            constraints = [
+                UniqueConstraint(
+                    Lower('name'),
+                    name='language_name_case_insensitive_unique',
+                    violation_error_message = "Language already exists (case insensitive match)"
+                ),
+            ]
