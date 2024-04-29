bf97be899cb899327dc39d892083e43cc02427cc
kiminzajnr <kiminzaeric@gmail.com>
2024-04-30 00:34:04 +0300

    customize admin

--- django_local_library/catalog/admin.py
+++ django_local_library/catalog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Genre, Book, BookInstance, Author, Language
 
-# Register your models here.
+
+admin.site.register(Genre)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
+# admin.site.register(Author)
+admin.site.register(Language)
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
