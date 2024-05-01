d5224f345b60f711a80c507a8c0e7dd4c6727975
kiminzajnr <kiminzaeric@gmail.com>
2024-05-01 15:40:20 +0300

    genre

--- django_local_library/catalog/views.py
+++ django_local_library/catalog/views.py
@@ -8,14 +8,16 @@ def index(request):
 
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
-
     num_authors = Author.objects.count()
+
+    num_genres_available = Genre.objects.filter(name='Fantasy').count()
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres_available': num_genres_available,
     }
 
     return render(request, 'index.html', context=context)
