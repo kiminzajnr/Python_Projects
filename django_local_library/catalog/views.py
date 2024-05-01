36cdbb065661c229f7c5152333bda5656cd85265
kiminzajnr <kiminzaeric@gmail.com>
2024-05-01 13:19:27 +0300

    index view

--- django_local_library/catalog/views.py
+++ django_local_library/catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from . models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
