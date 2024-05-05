bab180ec44bd646d7bf4b70d2ffca1313a69ea53
kiminzajnr <kiminzaeric@gmail.com>
2024-05-05 22:25:55 +0300

    list view

--- django_local_library/catalog/views.py
+++ django_local_library/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from . models import Book, Author, BookInstance, Genre
 
 
@@ -21,3 +22,6 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
