84f96c5788e7bba7256e4517c58e6145e65a2dbf
kiminzajnr <kiminzaeric@gmail.com>
2024-05-05 22:25:23 +0300

    url for book list view

--- django_local_library/catalog/urls.py
+++ django_local_library/catalog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name = 'books'),
 ]
