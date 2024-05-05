d312bd59c165baf7913f8a43418d6016c4880062
kiminzajnr <kiminzaeric@gmail.com>
2024-05-05 23:03:37 +0300

    url for detail

--- django_local_library/catalog/urls.py
+++ django_local_library/catalog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name = 'books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
