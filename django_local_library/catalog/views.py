790a78c7c0619d8f5f97dc8d3a78c6afa4026b4a
kiminzajnr <kiminzaeric@gmail.com>
2024-05-06 08:22:41 +0300

    add list and gen views

--- django_local_library/catalog/views.py
+++ django_local_library/catalog/views.py
@@ -25,7 +25,16 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
