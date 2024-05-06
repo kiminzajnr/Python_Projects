4c00b3aab8a0a7afbf9d17e4148a7c09138b975f
kiminzajnr <kiminzaeric@gmail.com>
2024-05-06 08:24:40 +0300

    urls

--- django_local_library/catalog/urls.py
+++ django_local_library/catalog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name = 'books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name = 'authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
