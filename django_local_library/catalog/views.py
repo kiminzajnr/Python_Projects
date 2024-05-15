586ac47da5b0dd6771df08474a048f3744542278
kiminzajnr <kiminzaeric@gmail.com>
2024-05-15 23:44:29 +0300

    count no of visits

--- django_local_library/catalog/views.py
+++ django_local_library/catalog/views.py
@@ -13,12 +13,16 @@ def index(request):
 
     num_genres_available = Genre.objects.filter(name='Fantasy').count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres_available': num_genres_available,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
