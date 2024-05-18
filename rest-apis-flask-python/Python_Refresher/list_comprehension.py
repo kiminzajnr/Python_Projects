d561a2d703016ef7ec30bbcdbf6da4849ccb1aa3
kiminzajnr <kiminzaeric@gmail.com>
2024-05-18 22:46:48 +0300

    simple example

--- rest-apis-flask-python/Python_Refresher/list_comprehension.py
+++ rest-apis-flask-python/Python_Refresher/list_comprehension.py
@@ -1,5 +1,13 @@
 numbers = [1, 3, 5]
 doubled = []
 
-for num in numbers:
-    doubled.append(num * 2)
+# for num in numbers:
+#     doubled.append(num * 2)
+
+# print(doubled)
+
+
+
+doubled = [num * 2 for num in numbers]
+
+print(doubled)
