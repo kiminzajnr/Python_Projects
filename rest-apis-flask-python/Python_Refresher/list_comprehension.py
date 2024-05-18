618136be2fc6347d96837223d6b07505d9042856
kiminzajnr <kiminzaeric@gmail.com>
2024-05-18 23:14:31 +0300

    list comprehension creates new list

--- rest-apis-flask-python/Python_Refresher/list_comprehension.py
+++ rest-apis-flask-python/Python_Refresher/list_comprehension.py
@@ -24,3 +24,5 @@ friends = ["Rolf", "Sam", "Samantha", "Saurabh", "jen"]
 
 starts_s = [friend for friend in friends if friend.startswith("S")]
 print(starts_s)
+
+print("friends: ", id(friends), "starts_s", id(starts_s))
