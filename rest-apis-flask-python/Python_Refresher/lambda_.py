8a9bae0da733da211e09f82fa0a1ff48ee56edb8
kiminzajnr <kiminzaeric@gmail.com>
2024-05-19 13:24:59 +0300

    map and lambda

--- rest-apis-flask-python/Python_Refresher/lambda_.py
+++ rest-apis-flask-python/Python_Refresher/lambda_.py
@@ -1,7 +1,11 @@
-def double(x):
-    return x * 2
+# def double(x):
+#     return x * 2
 
 sequence = [1, 3, 5, 9]
-doubled = [double(x) for x in sequence]
+# doubled = [double(x) for x in sequence]
+# doubled = map(double, sequence)
 
+# print(list(doubled))
+
+doubled = list(map(lambda x: x * 2, sequence))
 print(doubled)
