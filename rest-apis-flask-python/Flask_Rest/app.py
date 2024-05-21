488ddf7283c3ab7dbe1519a5dc28d72724af2421
kiminzajnr <kiminzaeric@gmail.com>
2024-05-21 22:26:13 +0300

    first endpoint

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -2,3 +2,20 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
+stores = [
+    {
+        "name": "My store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
