95e661f7d9a97fb57658b0c74ac6a1da6a383633
kiminzajnr <kiminzaeric@gmail.com>
2024-05-21 23:58:52 +0300

    add POST routes

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -19,3 +19,20 @@ stores = [
 @app.get("/store")
 def get_stores():
     return {"stores": stores}
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+    return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
