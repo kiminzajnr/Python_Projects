6075ba50dbb49d49b4bb217278895e1666ed1a3d
kiminzajnr <kiminzaeric@gmail.com>
2024-05-22 14:01:31 +0300

    add route to get a store/item

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -36,3 +36,17 @@ def create_item(name):
             store["items"].append(new_item)
             return new_item, 201
     return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
