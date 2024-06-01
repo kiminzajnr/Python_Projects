5cefd38a1ec7f95669b6b0e36bff3ba4d2efb3f1
kiminzajnr <kiminzaeric@gmail.com>
2024-06-01 18:21:35 +0300

    add delete and update endpoint

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -72,3 +72,29 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
          abort(404, message=f"item not found")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_id = request.get_json()
+    item_data = request.get_json()
+
+    if "price" not in item_data or "name" not in item_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
+        )
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
