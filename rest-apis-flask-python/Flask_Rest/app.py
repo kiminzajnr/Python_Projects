26516281fda5512775fef4440318dbabad8ea995
kiminzajnr <kiminzaeric@gmail.com>
2024-06-01 13:44:31 +0300

    add error handling

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -17,16 +17,34 @@ def create_store():
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}
     stores[store_id] = store
+
     return store, 201
 
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        abort(404, {"message": "Store not found"})
+    
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+    
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
+
+    return item
 
 
 @app.get("/item")
@@ -38,11 +56,11 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-         abort(404, {"message": "Store not found"})
+         abort(404, message={"message": "Store not found"})
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-         abort(404, {"message": "item not found"})
+         abort(404, message={"message": "item not found"})
