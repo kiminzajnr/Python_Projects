1aacc7934ca9b99a85b266dab74865a4eda15321
kiminzajnr <kiminzaeric@gmail.com>
2024-06-01 11:38:48 +0300

    use abort for easier documentation

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 
 
@@ -22,7 +23,7 @@ def create_store():
 def create_item():
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, {"message": "Store not found"})
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
     items[item_id] = item
@@ -37,11 +38,11 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+         abort(404, {"message": "Store not found"})
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+         abort(404, {"message": "item not found"})
