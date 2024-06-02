3d0887f71a703695248ab1a628d599fc5b1c2c13
kiminzajnr <kiminzaeric@gmail.com>
2024-06-02 20:08:47 +0300

    validate stores with marshmallow

--- rest-apis-flask-python/Flask_Rest/resources/store.py
+++ rest-apis-flask-python/Flask_Rest/resources/store.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
@@ -27,13 +28,8 @@ class StoreList(MethodView):
     def get(self):
         return {"stores": list(stores.values())}
     
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload.",
-            )
+    @blp.arguments(StoreSchema)
+    def post(self, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message=f"Store already exists.")
