fe845ef4f69f57cbbd6f88023ddda234b5022411
kiminzajnr <kiminzaeric@gmail.com>
2024-06-08 00:23:55 +0300

    update:schemas

--- rest-apis-flask-python/Flask_Rest/schemas.py
+++ rest-apis-flask-python/Flask_Rest/schemas.py
@@ -1,15 +1,21 @@
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+class PlainItemSchema(Schema):
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()   
+
+class ItemSchema(Schema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
 class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
