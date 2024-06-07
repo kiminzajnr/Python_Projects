0ba34e1590e0a6895d32cb1d58377855de11df36
kiminzajnr <kiminzaeric@gmail.com>
2024-06-08 00:11:00 +0300

    add: stores

--- rest-apis-flask-python/Flask_Rest/models/item.py
+++ rest-apis-flask-python/Flask_Rest/models/item.py
@@ -8,3 +8,8 @@ class ItemModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, unique=False, nullable=False)
+
+    store_id = db.Column(
+        db.Iteger, db.ForeignKey("stores.id"), unique=False, nullable=False
+    )
+    store = db.relationship("StoreModel", back_populates="items")
