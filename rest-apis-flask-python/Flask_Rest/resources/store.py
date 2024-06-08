90759e0f35fdebcd26c2b42fd598b89236f45d36
kiminzajnr <kiminzaeric@gmail.com>
2024-06-08 18:06:29 +0300

    -

--- rest-apis-flask-python/Flask_Rest/resources/store.py
+++ rest-apis-flask-python/Flask_Rest/resources/store.py
@@ -2,11 +2,33 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from db import stores
+from db import db
 from schemas import StoreSchema
+
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
+from models import StoreModel
 
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
+
+
+@blp.arguments(StoreSchema)
+@blp.response(201, StoreSchema)
+def post(self, store_data):
+    store = StoreModel(**store_data)
+    try:
+        db.session.add(store)
+        db.session.commit()
+    except IntegrityError:
+        abort(
+            400,
+            message="A store with that name already exists.",
+        )
+    except SQLAlchemyError:
+        abort(500, message="An error occurred creating the store.")
+
+    return store
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
