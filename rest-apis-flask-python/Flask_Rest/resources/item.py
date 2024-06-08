d6ba4db0203a8742551f9f1295a4086192cdfe6f
kiminzajnr <kiminzaeric@gmail.com>
2024-06-08 18:06:03 +0300

    add: item

--- rest-apis-flask-python/Flask_Rest/resources/item.py
+++ rest-apis-flask-python/Flask_Rest/resources/item.py
@@ -4,42 +4,43 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from schemas import ItemSchema, ItemUpdateSchema
 
-from db import items
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
+@blp.arguments(ItemSchema)
+@blp.response(201, ItemSchema)
+def post(self, item_data):
+    item = ItemModel(**item_data)
+
+    try:
+        db.session.add(item)
+        db.session.commit()
+    except SQLAlchemyError:
+        abort(500, message="An error occured while inserting the item.")
+
+    return item
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self, item_id):
-        try:
-            return items[item_id]
-        except KeyError:
-            abort(404, message="Item not found.")
+        item = ItemModel.query.get_or_404(item_id)
+        return item
 
     def delete(self, item_id):
-        try:
-            del items[item_id]
-            return {"message": "Item deleted."}
-        except KeyError:
-            abort(404, message="Item not found.")
-
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented.")
+    
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        
-        try:
-            item = items[item_id]
-
-            # https://blog.teclado.com/python-dictionary-merge-update-operators/
-            item |= item_data
-
-            return item
-        except KeyError:
-            abort(404, message="Item not found.")
-
-
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Updating an item is not implemented.")
+    
 @blp.route("/item")
 class ItemList(MethodView):
     @blp.response(201, ItemSchema)
