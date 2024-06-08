d333fc420930cba6d8a600e0a5fdbba440de9e1f
kiminzajnr <kiminzaeric@gmail.com>
2024-06-08 18:17:57 +0300

    update: models sqlalchemy

--- rest-apis-flask-python/Flask_Rest/resources/item.py
+++ rest-apis-flask-python/Flask_Rest/resources/item.py
@@ -38,8 +38,17 @@ class Item(MethodView):
     @blp.arguments(ItemUpdateSchema)
     @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
-        item = ItemModel.query.get_or_404(item_id)
-        raise NotImplementedError("Updating an item is not implemented.")
+        item = ItemModel.query.get(item_id)
+        if item:
+            item.price = item_data["price"]
+            item.name = item_data["name"]
+        else:
+            item = ItemModel(id=item_id, **item_data)
+
+        db.session.add(item)
+        db.session.commit()
+
+        return item
     
 @blp.route("/item")
 class ItemList(MethodView):
