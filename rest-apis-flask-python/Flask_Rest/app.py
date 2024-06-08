2b550b211b4cef66ff62af5ca3c3278b503625b8
kiminzajnr <kiminzaeric@gmail.com>
2024-06-08 13:08:07 +0300

    configure: flask sqlalchemy

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -1,4 +1,5 @@
 import models
+import os
 from db import db
 
 from flask import Flask
@@ -7,7 +8,7 @@ from flask_smorest import Api
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -17,10 +18,15 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
     api = Api(app)
+
+    with app.app_context():
+        db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
-
+    
     return app
