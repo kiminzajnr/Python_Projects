ce9e98be38312694887f954f12ea4483e10f8364
kiminzajnr <kiminzaeric@gmail.com>
2024-06-14 12:29:47 +0300

    add: POST tag

--- rest-apis-flask-python/Flask_Rest/resources/tag.py
+++ rest-apis-flask-python/Flask_Rest/resources/tag.py
@@ -1,9 +1,14 @@
 from flask.views import MethodView
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
+
+from sqlalchemy.exc import SQLAlchemyError
 
 from schemas import TagSchema
 
+from db import db
+
 from models import StoreModel
+from models import TagModel
 
 
 blp = Blueprint("Tags", "tags", description="Operation on tags")
@@ -16,3 +21,21 @@ class TagsInStore(MethodView):
         store = StoreModel.query.get_or_404(store_id)
 
         return store.tags.all()
+    
+    @blp.arguments(TagSchema)
+    @blp.response(201, TagSchema)
+    def post(self, tag_data, store_id):
+        if TagModel.query.filter(TagModel.store_id == store_id, TagModel.name == tag_data["name"].first()):
+            abort(400, message="A tag with that name already exists in that store")
+        tag = TagModel(**tag_data, store_id=store_id)
+
+        try:
+            db.session.add(tag)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            abort(
+                500,
+                message=str(e)
+            )
+
+        return tag
