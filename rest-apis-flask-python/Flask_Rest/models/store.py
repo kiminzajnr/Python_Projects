e814194d9b9f7e796ab8d6568d6c2e8c4b8051c4
kiminzajnr <kiminzaeric@gmail.com>
2024-06-14 11:36:11 +0300

    add: store relationship with tags

--- rest-apis-flask-python/Flask_Rest/models/store.py
+++ rest-apis-flask-python/Flask_Rest/models/store.py
@@ -8,3 +8,4 @@ class StoreModel(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
 
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
+    tags = db.relationship("TagModel", back_populates="store", lazy="dynamic")
