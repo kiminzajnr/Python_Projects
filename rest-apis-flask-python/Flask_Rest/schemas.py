8d21e65a1e397f7b762cae7ff061f240b427c4ee
kiminzajnr <kiminzaeric@gmail.com>
2024-06-14 11:45:40 +0300

    add: marshmallow schemas

--- rest-apis-flask-python/Flask_Rest/schemas.py
+++ rest-apis-flask-python/Flask_Rest/schemas.py
@@ -7,7 +7,11 @@ class PlainItemSchema(Schema):
 
 class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
-    name = fields.Str()   
+    name = fields.Str()
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
@@ -20,3 +24,8 @@ class ItemUpdateSchema(Schema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainItemSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
