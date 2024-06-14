f300b3deda45df5e6e85c5b929b6b85668f76323
kiminzajnr <kiminzaeric@gmail.com>
2024-06-14 21:03:05 +0300

    add schemas

--- rest-apis-flask-python/Flask_Rest/schemas.py
+++ rest-apis-flask-python/Flask_Rest/schemas.py
@@ -16,6 +16,7 @@ class PlainTagSchema(Schema):
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
@@ -26,6 +27,12 @@ class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
-class TagSchema(PlainItemSchema):
+class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
