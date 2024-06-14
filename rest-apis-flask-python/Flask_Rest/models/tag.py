ad225c30064cd0347f27e6dfd2734d2d101ca59d
kiminzajnr <kiminzaeric@gmail.com>
2024-06-14 20:43:41 +0300

    use secondary table in main models

--- rest-apis-flask-python/Flask_Rest/models/tag.py
+++ rest-apis-flask-python/Flask_Rest/models/tag.py
@@ -9,3 +9,4 @@ class TagModel(db.Model):
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
 
     store = db.relationship("StoreModel", back_populates="tags")
+    items = db.relationship("ItemModel", back_populates="tags", secondary="items_tags")
