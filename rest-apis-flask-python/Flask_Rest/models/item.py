ea55081dce5a5d9d1d1d16d90cb17378cbc6990d
kiminzajnr <kiminzaeric@gmail.com>
2024-06-15 13:47:01 +0300

    add tag field

--- rest-apis-flask-python/Flask_Rest/models/item.py
+++ rest-apis-flask-python/Flask_Rest/models/item.py
@@ -12,3 +12,5 @@ class ItemModel(db.Model):
         db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False
     )
     store = db.relationship("StoreModel", back_populates="items")
+
+    tags = db.relationship("TagModel", back_populates="items", secondary="items_tags")
