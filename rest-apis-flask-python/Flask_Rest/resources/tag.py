599f3d27f5a53550bd7327959a2b1e42de2a1aca
kiminzajnr <kiminzaeric@gmail.com>
2024-06-15 13:00:06 +0300

    add DELETE tag

--- rest-apis-flask-python/Flask_Rest/resources/tag.py
+++ rest-apis-flask-python/Flask_Rest/resources/tag.py
@@ -4,6 +4,7 @@ from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
 from schemas import TagSchema
+from schemas import TagAndItemSchema
 
 from db import db
 
@@ -57,6 +58,20 @@ class LinkTagsToItem(MethodView):
             abort(500, message="An error occurred while inserting the tag.")
 
         return tag
+    
+    @blp.response(200, TagAndItemSchema)
+    def delete(self, item_id, tag_id):
+        item = ItemModel.query.get_or_404(item_id)
+        tag = TagModel.query.get_or_404(tag_id)
+
+        item.tags.remove(tag)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500, message="An error occurred while inserting the tag.")
+        return {"message": "Item removed from tag", "item": item, "tag": tag}
     
 @blp.route("/tag/<string:tag_id>")
 class Tag(MethodView):
