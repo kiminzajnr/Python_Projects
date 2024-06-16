11a80a54caa36f18fc3cc950907715b48a2582de
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 20:03:23 +0300

    error handling'

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -1,6 +1,7 @@
 import os
 
 from db import db
+from blocklist import BLOCKLIST
 
 from flask import Flask, jsonify
 from flask_smorest import Api
@@ -61,6 +62,18 @@ def create_app(db_url=None):
                     "description": "Request does not contain an access token.",
                     "error": "authorization_required"
                 }
+            ),
+            401,
+        )
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+    
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
             ),
             401,
         )
