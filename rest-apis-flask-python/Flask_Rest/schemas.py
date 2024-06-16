f80614317e69b8a2de668ac88a96dfbb7f7e4cca
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 12:19:31 +0300

    add: user schema

--- rest-apis-flask-python/Flask_Rest/schemas.py
+++ rest-apis-flask-python/Flask_Rest/schemas.py
@@ -36,3 +36,8 @@ class TagAndItemSchema(Schema):
     message = fields.Str()
     item = fields.Nested(ItemSchema)
     tag = fields.Nested(TagSchema)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
