09a78cdba9098895a969727cf22e3500e3fac705
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 13:21:49 +0300

    fix TypeError

--- rest-apis-flask-python/Flask_Rest/resources/user.py
+++ rest-apis-flask-python/Flask_Rest/resources/user.py
@@ -12,7 +12,7 @@ blp = Blueprint("Users", "users", description="Operations on users")
 
 
 @blp.route("/register")
-class UserRegister("/register"):
+class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     def post(self, user_data):
         if UserModel.query.filter(UserModel.username == user_data["username"]).first():
