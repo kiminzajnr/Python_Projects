e79c3a1a4c0ed6ed845d28acb18a95094476f259
kiminzajnr <kiminzaeric@gmail.com>
2024-06-17 01:13:32 +0300

    error handling for fresh token

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -77,6 +77,18 @@ def create_app(db_url=None):
             ),
             401,
         )
+    
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
 
     with app.app_context():
         db.create_all()
