b594152714ff315c03790454a1bc252c8f3d9b47
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 15:52:18 +0300

    jwt for post item

--- rest-apis-flask-python/Flask_Rest/resources/item.py
+++ rest-apis-flask-python/Flask_Rest/resources/item.py
@@ -13,11 +13,13 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema)
     def get(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         return item
 
+    @jwt_required()
     def delete(self, item_id):
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
@@ -38,9 +40,10 @@ class Item(MethodView):
         db.session.commit()
 
         return item
-    
+  
 @blp.route("/item")
 class ItemList(MethodView):
+    @jwt_required()
     @blp.response(200, ItemSchema(many=True))
     def get(self):
         return ItemModel.query.all()
