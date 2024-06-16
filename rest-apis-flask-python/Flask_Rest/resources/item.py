f49280b33b4d3ab9196d2acb5971a7d5c9f9de20
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 19:14:54 +0300

    _

--- rest-apis-flask-python/Flask_Rest/resources/item.py
+++ rest-apis-flask-python/Flask_Rest/resources/item.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt
 from schemas import ItemSchema, ItemUpdateSchema
 
 from sqlalchemy.exc import SQLAlchemyError
@@ -21,6 +21,9 @@ class Item(MethodView):
 
     @jwt_required()
     def delete(self, item_id):
+        jwt = get_jwt()
+        if not jwt.get("is_admin"):
+            abort(401, message="Admin privilege required.")
         item = ItemModel.query.get_or_404(item_id)
         db.session.delete(item)
         db.session.commit()
