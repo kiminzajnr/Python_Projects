4baa3e906a26276434af43235d5fbf2216717cc0
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 15:19:59 +0300

    add login endpoint'

--- rest-apis-flask-python/Flask_Rest/resources/user.py
+++ rest-apis-flask-python/Flask_Rest/resources/user.py
@@ -1,5 +1,6 @@
 from flask_smorest import Blueprint, abort
 from flask.views import MethodView
+from flask_jwt_extended import create_access_token
 from passlib.hash import pbkdf2_sha256
 
 from schemas import UserSchema
@@ -26,6 +27,20 @@ class UserRegister(MethodView):
         db.session.commit()
 
         return {"message": "User created successfully."}, 201
+    
+@blp.route("/login")
+class UserLogin(MethodView):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(
+            UserModel.username == user_data["username"]
+        ).first()
+        
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password):
+            access_token = create_access_token(identity=user.id)
+            return {"access_token": access_token}
+        
+        abort(401, message="Invalid credentials.")
     
 @blp.route("/user/<int:user_id>")
 class User(MethodView):
