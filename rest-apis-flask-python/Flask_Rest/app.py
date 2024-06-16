4b2f63144716fe401c022cad012302b1d1a8eb06
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 13:12:17 +0300

    register bluepring

--- rest-apis-flask-python/Flask_Rest/app.py
+++ rest-apis-flask-python/Flask_Rest/app.py
@@ -9,6 +9,7 @@ from flask_jwt_extended import JWTManager
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -36,5 +37,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
     
     return app
