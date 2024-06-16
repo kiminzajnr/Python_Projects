43bf935c347130681b8efc991a4dda4ee08db3ca
kiminzajnr <kiminzaeric@gmail.com>
2024-06-17 01:09:30 +0300

    require fresh token for item POST

--- rest-apis-flask-python/Flask_Rest/resources/item.py
+++ rest-apis-flask-python/Flask_Rest/resources/item.py
@@ -51,7 +51,7 @@ class ItemList(MethodView):
     def get(self):
         return ItemModel.query.all()
 
-    @jwt_required()
+    @jwt_required(fresh=True)
     @blp.arguments(ItemSchema)
     @blp.response(201, ItemSchema)
     def post(self, item_data):
