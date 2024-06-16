acc195e00d4b3a1def281221e98bd9035db296ff
kiminzajnr <kiminzaeric@gmail.com>
2024-06-16 13:02:15 +0300

    add: register user endpoint

---
+++ rest-apis-flask-python/Flask_Rest/resources/user.py
@@ -0,0 +1,27 @@
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+
+from schemas import UserSchema
+
+from models import UserModel
+from db import db
+
+
+blp = Blueprint("Users", "users", description="Operations on users")
+
+
+@blp.route("/register")
+class UserRegister("/register"):
+    @blp.arguments(UserSchema)
+    def post(self, user_data):
+        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+            abort(409, message="A user with that username already exists.")
+
+        user = UserModel(
+            username=user_data["username"],
+            password=pbkdf2_sha256.hash(user_data["password"]),
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        return {"message": "User created successfully."}, 201
