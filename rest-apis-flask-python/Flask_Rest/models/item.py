5c095693e0baedc2717acb72b645a28596a67540
kiminzajnr <kiminzaeric@gmail.com>
2024-06-21 21:17:40 +0300

    change models to test alembic

--- rest-apis-flask-python/Flask_Rest/models/item.py
+++ rest-apis-flask-python/Flask_Rest/models/item.py
@@ -6,6 +6,7 @@ class ItemModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
 
     store_id = db.Column(
