c7119779f3a2aeb5758edc057e70a3736fad7e98
kiminzajnr <kiminzaeric@gmail.com>
2024-07-10 13:41:25 +0300

    Decouple data from display

--- progdiary/app.py
+++ progdiary/app.py
@@ -1,4 +1,4 @@
-from db import add_entry, view_entries
+from db import add_entry, get_entries
 
 
 menu = """Please select one of the following options:
@@ -9,12 +9,23 @@ menu = """Please select one of the following options:
 Your selection: """
 welcome = "Welcome to the programming diary!"
 
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+
+
 print(welcome)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        add_entry()
+        prompt_new_entry()
     elif user_input == "2":
-        view_entries()
+        view_entries(get_entries())
     else:
         print("Invalid option, please try again!")
