0a2e4fe94101aae1184471c1334e74f0c2a4a083
kiminzajnr <kiminzaeric@gmail.com>
2024-07-10 13:22:41 +0300

    Use the databse in app.py

--- progdiary/app.py
+++ progdiary/app.py
@@ -1,3 +1,6 @@
+from db import add_entry, view_entries
+
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -10,8 +13,8 @@ print(welcome)
 
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        print("Adding...")
+        add_entry()
     elif user_input == "2":
-        print("Viewing...")
+        view_entries()
     else:
         print("Invalid option, please try again!")
