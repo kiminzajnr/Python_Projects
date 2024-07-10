0571d4e07a97fe828c65c4e0fa6e3e18fd48d4f9
kiminzajnr <kiminzaeric@gmail.com>
2024-07-10 13:41:18 +0300

    Decouple data from display

--- progdiary/db.py
+++ progdiary/db.py
@@ -1,12 +1,8 @@
 entries = []
 
-def add_entry():
-    entry_content = input("What have you learned today? ")
-    entry_date = input("Enter the date: ")
-
+def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
 
 
-def view_entries():
-    for entry in entries:
-        print(f"{entry['date']}\n{entry['content']}\n\n")
+def get_entries():
+    return entries
