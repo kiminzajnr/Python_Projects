3753eb4f92616df323a947bc70c0dafa87680b36
kiminzajnr <kiminzaeric@gmail.com>
2024-07-14 17:00:58 +0300

    Connect to SQLite

--- progdiary/db.py
+++ progdiary/db.py
@@ -1,4 +1,11 @@
-entries = []
+import sqlite3
+
+
+connection = sqlite3.connect("data.db")
+
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE entries (context TEXT, date TEXT);")
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
