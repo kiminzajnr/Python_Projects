71d364c346ef7bd0970e883a24e6723899b909cc
kiminzajnr <kiminzaeric@gmail.com>
2024-07-14 17:59:21 +0300

    Insert data into SQLite with Python

--- progdiary/db.py
+++ progdiary/db.py
@@ -5,10 +5,13 @@ connection = sqlite3.connect("data.db")
 
 def create_table():
     with connection:
-        connection.execute("CREATE TABLE entries (context TEXT, date TEXT);")
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (context TEXT, date TEXT);")
 
 def add_entry(entry_content, entry_date):
-    entries.append({"content": entry_content, "date": entry_date})
+    with connection:
+        connection.execute(
+            "INSERT INTO entries VALUES (?, ?);", (entry_content, entry_date)
+        )
 
 
 def get_entries():
