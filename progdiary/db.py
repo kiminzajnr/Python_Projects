720d60fdfd565538b715960105773bffe34ec8d8
kiminzajnr <kiminzaeric@gmail.com>
2024-07-14 18:37:38 +0300

    Add Retrieve data from SQLite queries

--- progdiary/db.py
+++ progdiary/db.py
@@ -2,10 +2,11 @@ import sqlite3
 
 
 connection = sqlite3.connect("data.db")
+connection.row_factory = sqlite3.Row
 
 def create_table():
     with connection:
-        connection.execute("CREATE TABLE IF NOT EXISTS entries (context TEXT, date TEXT);")
+        connection.execute("CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT);")
 
 def add_entry(entry_content, entry_date):
     with connection:
@@ -15,4 +16,5 @@ def add_entry(entry_content, entry_date):
 
 
 def get_entries():
-    return entries
+    cursor = connection.execute("SELECT * FROM entries;")
+    return cursor
