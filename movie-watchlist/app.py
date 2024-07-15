63bcf6551693d5fbad49ee65856e0127552d0f46
kiminzajnr <kiminzaeric@gmail.com>
2024-07-15 23:55:56 +0300

    Add project starter code

---
+++ movie-watchlist/app.py
@@ -0,0 +1,30 @@
+import database
+
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3) View all movies
+4) Watch a movie
+5) View watched movies.
+6) Exit.
+
+Your selection: """
+welcome = "Welcome to the watchlist app!"
+
+
+print(welcome)
+database.create_tables()
+
+while (user_input := input(menu)) != "6":
+    if user_input == "1":
+        pass
+    elif user_input == "2":
+        pass
+    elif user_input == "3":
+        pass
+    elif user_input == "4":
+        pass
+    elif user_input == "5":
+        pass
+    else:
+        print("Invalid input, please try again!")
