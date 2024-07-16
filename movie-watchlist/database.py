c6d4a708e1fb527ff7fc533ed0dfc588e64cf176
kiminzajnr <kiminzaeric@gmail.com>
2024-07-16 23:30:38 +0300

    Implement moview watched

--- movie-watchlist/database.py
+++ movie-watchlist/database.py
@@ -11,6 +11,7 @@ INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
 
 
 connection = sqlite3.connect("data.db")
@@ -38,7 +39,8 @@ def get_movies(upcoming=False):
 
 
 def watch_movies(movie_title):
-    pass
+    with connection:
+        connection.execute(SET_MOVIE_WATCHED, (movie_title, ))
 
 def get_watched_movies():
     with connection:
