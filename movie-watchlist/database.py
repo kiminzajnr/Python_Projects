37be2daf88b8e7e026ea9e7b9af8da4a04410f77
kiminzajnr <kiminzaeric@gmail.com>
2024-07-16 22:52:46 +0300

    Add function definitions

--- movie-watchlist/database.py
+++ movie-watchlist/database.py
@@ -1,3 +1,6 @@
+import datetime
+import sqlite3
+
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
     title TEXT,
     release_timestamp REAL,
@@ -8,3 +11,25 @@ INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp, watched) VALUES (
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
 SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = "SELECT * FROM movies WHERE watched = 1;"
+
+
+connection = sqlite3.connect("data.db")
+
+
+def create_tables():
+    pass
+
+
+def add_movie(title, release_timestamp)
+    pass
+
+
+def get_movies(upcoming=False):
+    pass
+
+
+def watch_movies(movie_title):
+    pass
+
+def get_watched_movies():
+    pass
