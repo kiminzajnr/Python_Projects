387993d1a9652f3e67e08f9e0184994c9cbaa280
kiminzajnr <kiminzaeric@gmail.com>
2024-07-18 21:29:39 +0300

    Add view upcoming movies

--- movie-watchlist/app.py
+++ movie-watchlist/app.py
@@ -26,11 +26,19 @@ def prompt_add_movie():
 
     database.add_movie(title, timestamp)
 
+
+def print_movie_list(movies):
+    print("-- Upcoming movies --")
+    for movie in movies:
+        print(f"{movie[0]} (on {movie[1]})")
+    print("--- \n")
+
 while (user_input := input(menu)) != "6":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
-        pass
+        movies = database.get_movies(True)
+        print_movie_list(movies)
     elif user_input == "3":
         pass
     elif user_input == "4":
