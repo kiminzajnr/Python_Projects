f1a3f6c78f88f69cc970bbc5ae269e07172c2e99
kiminzajnr <kiminzaeric@gmail.com>
2024-07-18 23:05:05 +0300

    Add Get watched movies by username

--- movie-watchlist/app.py
+++ movie-watchlist/app.py
@@ -36,6 +36,13 @@ def print_movie_list(heading, movies):
     print("--- \n")
 
 
+def print_watched_movie_list(username, movies):
+    print(f"-- {username}'s watched moview --")
+    for movie in movies:
+        print(f"{movie[1]}")
+    print("---- \n")
+
+
 def prompt_watch_movie():
     username = input("Username: ")
     movie_title = input("Enter movie title you've watched: ")
@@ -54,7 +61,8 @@ while (user_input := input(menu)) != "6":
     elif user_input == "4":
         prompt_watch_movie()
     elif user_input == "5":
-        movies = database.get_watched_movies()
-        print_movie_list("Watched", movies)
+        username = input("Username: ")
+        movies = database.get_watched_movies(username)
+        print_movie_list(username, movies)
     else:
         print("Invalid input, please try again!")
