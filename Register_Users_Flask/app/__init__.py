76f7bae22e77a4ad37ee7662eac2356e1c52c192
kiminzajnr <kiminzaeric@gmail.com>
2024-09-01 00:07:51 +0300

    Feat: Implement signup and login

--- Register_Users_Flask/app/__init__.py
+++ Register_Users_Flask/app/__init__.py
@@ -9,6 +9,7 @@ from flask import (
     redirect,
     url_for,
 )
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 # Secret key generated with secrets.token_urlsafe()
@@ -19,6 +20,7 @@ users = {}
 
 @app.get("/")
 def home():
+    # session.clear()
     return render_template("home.html", email=session.get("email"))
 
 
@@ -29,11 +31,29 @@ def protected():
     return render_template("protected.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    email = ""
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if pbkdf2_sha256.verify(password, users.get(email)):
+            session["email"] = email
+            return redirect(url_for("protected"))
+        flash("Incorrect e-mail or password.")
+    return render_template("login.html", email=email)
 
 
-@app.route("/signup", methods=["GET"])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        users[email] = pbkdf2_sha256.hash(password)
+        # print(users)
+        
+        flash("Successfully signed up.")
+        return redirect(url_for("login"))
+        
     return render_template("signup.html")
