e1a4c93d0ebdf9d8b74db0a00d1268392fae48db
kiminzajnr <kiminzaeric@gmail.com>
2024-10-01 18:33:37 +0300

    Ratings histogram

---
+++ SparkCourse/ratings-counter.py
@@ -0,0 +1,19 @@
+from pyspark import SparkConf, SparkContext
+import collections
+
+conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+total = 0
+
+lines = sc.textFile("/Users/kimiza/Desktop/Desktop/Python_Projects/SparkCourse/ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
+    total += value
+
+print("+++++++++++++++++++++++++++")
+print(total)
